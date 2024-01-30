ab62efeed3a5a6c66ad106d50f7d6a4ee16c2cd4
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-30 15:21:04 +0800

    get coupon

---
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,55 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CouponController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return null;
+        }
+
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.First(u => u.CouponId == id);
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return null;
+        }
+
+    }
+}
