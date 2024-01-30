a3acb3d3d635f49ad2b24cbf95c116695bad4293
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-30 15:29:34 +0800

    Common Response

--- Mango.Services.CouponAPI/Controllers/CouponController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -1,6 +1,6 @@
 ﻿using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
-using Microsoft.AspNetCore.Http;
+using Mango.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.CouponAPI.Controllers
@@ -10,45 +10,47 @@ namespace Mango.Services.CouponAPI.Controllers
     public class CouponController : ControllerBase
     {
         private readonly AppDbContext _db;
+        private ResponseDto _response;
 
         public CouponController(AppDbContext db)
         {
             _db = db;
+            _response = new ResponseDto();
         }
 
 
         [HttpGet]
-        public object Get()
+        public ResponseDto Get()
         {
             try
             {
                 IEnumerable<Coupon> objList = _db.Coupons.ToList();
-                return objList;
+                _response.Result = objList;
             }
             catch (Exception ex)
             {
-
-                throw;
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
             }
-            return null;
+            return _response;
         }
 
 
         [HttpGet]
         [Route("{id:int}")]
-        public object Get(int id)
+        public ResponseDto Get(int id)
         {
             try
             {
-                Coupon objList = _db.Coupons.First(u => u.CouponId == id);
-                return objList;
+                Coupon obj = _db.Coupons.First(u => u.CouponId == id);
+                _response.Result = obj;
             }
             catch (Exception ex)
             {
-
-                throw;
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
             }
-            return null;
+            return _response;
         }
 
     }
