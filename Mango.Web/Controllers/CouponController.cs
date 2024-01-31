776cddfd1d3d497b8697bb7a2779259b0ad2dce5
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-31 15:38:05 +0800

    CouponIndex

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,31 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new List<CouponDto>();
+            ResponseDto? response = await _couponService.GetAllCouponAsync();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+    }
+}
