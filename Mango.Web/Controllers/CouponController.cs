d8c74cb83dd0918d2a896929a30c7f3fb548afa9
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-31 15:58:17 +0800

    Create coupon page

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -27,5 +27,13 @@ namespace Mango.Web.Controllers
 
             return View(list);
         }
+
+
+        public async Task <IActionResult> CouponCreate()
+        {
+
+            return View();
+        }
+
     }
 }
