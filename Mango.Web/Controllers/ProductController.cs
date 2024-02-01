d14a81575d13d500e17e74a68151374baa22ff8b
liupenghao@fulan.com.cn <haochen214@163.com>
2024-02-01 17:55:01 +0800

    ProductController

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+
+
+        public IActionResult ProductIndex()
+        {
+            return View();
+        }
+
+        public IActionResult ProductCreate()
+        {
+            return View();
+        }
+
+        public IActionResult ProductEdit()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDelete()
+        {
+            return View();
+        }
+    }
+}
