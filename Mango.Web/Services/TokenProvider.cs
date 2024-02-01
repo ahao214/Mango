bc1d4cb7fda5c1392e27ae82f8218563eac6de89
liupenghao@fulan.com.cn <haochen214@163.com>
2024-02-01 14:58:08 +0800

    ITokenProvider

---
+++ Mango.Web/Services/TokenProvider.cs
@@ -0,0 +1,48 @@
+﻿using Mango.Web.Services.IServices;
+using Mango.Web.Utility;
+using Newtonsoft.Json.Linq;
+
+namespace Mango.Web.Services
+{
+    public class TokenProvider : ITokenProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TokenProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// 清空Token
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void ClearToken()
+        {
+            _httpContextAccessor.HttpContext?.Response.Cookies.Delete(SD.TokenCookie);
+        }
+
+        /// <summary>
+        /// 获取Token
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public string? GetToken()
+        {
+            string? token = null;
+            bool? hasToken = _httpContextAccessor.HttpContext?.Request.Cookies.TryGetValue(SD.TokenCookie, out token);
+
+            return hasToken is true ? token : null;
+        }
+
+        /// <summary>
+        /// 设置Token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void SetToken(string token)
+        {
+            _httpContextAccessor.HttpContext?.Response.Cookies.Append(SD.TokenCookie, token);
+        }
+    }
+}
