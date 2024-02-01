c971e7a3ba9f1fccd10e65afe52a609a6e5e88e7
liupenghao@fulan.com.cn <haochen214@163.com>
2024-02-01 18:27:25 +0800

    ProductService

---
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<ResponseDto?> GetProductAsync(string productName);
+        Task<ResponseDto?> GetAllProductAsync();
+        Task<ResponseDto?> GetProductByIdAsync(int id);
+        Task<ResponseDto?> CreateProductsAsync(ProductDto productDto);
+        Task<ResponseDto?> UpdateProductsAsync(ProductDto productDto);
+        Task<ResponseDto?> DeleteProductsAsync(int id);
+    }
+}
